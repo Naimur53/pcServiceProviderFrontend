b123003b8578558df141ac98a806fc70f501f396
naimur53 <naimurrhman53@gmail.com>
2023-11-12 23:57:01 +0600

    done

--- src/components/HomeCompo/WhyChooseUs.tsx
+++ src/components/HomeCompo/WhyChooseUs.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { smoothShowAnimation } from "@/utils/animation";
 
 type Props = {};
 
@@ -8,14 +9,22 @@ const WhyChooseUs = (props: Props) => {
     <div className="mt-20 pb-20">
       <div className="container mb-10">
         <h2 className="text-4xl font-bold text-center">Why Choose Us</h2>
-        <p className="text-center mt-2">
-          We have highly skills people and many more
-        </p>
+        <p className="text-center mt-2">We have highly skills people</p>
       </div>
-      <div className="container mx-auto max-w-5xl pt-10 flex gap-12 flex-wrap items-start justify-center md:justify-between">
-        <div className="grid gap-4 justify-items-center text-center md:flex-1">
-          <div className=" rounded-full border-8 border-main-primary p-4 relative w-[120px] h-[120px]">
-            <motion.svg
+      <motion.div
+        variants={smoothShowAnimation.container}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false }}
+        transition={{ type: "just" }}
+        className="container mx-auto max-w-5xl pt-10 flex gap-12 flex-wrap items-start justify-center md:justify-between"
+      >
+        <motion.div
+          variants={smoothShowAnimation.item}
+          className="grid gap-4 justify-items-center text-center md:flex-1"
+        >
+          <div className=" rounded-full border-8 border-main-primary p-4 relative  ">
+            <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -23,19 +32,22 @@ const WhyChooseUs = (props: Props) => {
               stroke="currentColor"
               // initial={{ scale: 1 }}
               // animate={{ scale: 1 }}
-              className="w-14 h-14 absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 right-1/2 "
+              className="w-14 h-14  "
             >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
               ></path>
-            </motion.svg>
+            </svg>
           </div>
           <h3 className="text-3xl font-bold">Safe</h3>
           <p>Our products are secure and private out-of-the-box</p>
-        </div>
-        <div className="grid gap-4 justify-items-center text-center md:flex-1">
+        </motion.div>
+        <motion.div
+          variants={smoothShowAnimation.item}
+          className="grid gap-4 justify-items-center text-center md:flex-1"
+        >
           <div className=" rounded-full border-8 border-main-primary p-4 ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -54,8 +66,11 @@ const WhyChooseUs = (props: Props) => {
           </div>
           <h3 className="text-3xl font-bold">Efficient</h3>
           <p>Feel good about your wallet and the environment</p>
-        </div>
-        <div className="grid gap-4 justify-items-center text-center md:flex-1">
+        </motion.div>
+        <motion.div
+          variants={smoothShowAnimation.item}
+          className="grid gap-4 justify-items-center text-center md:flex-1"
+        >
           <div className=" rounded-full border-8 border-main-primary p-4 ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -74,8 +89,8 @@ const WhyChooseUs = (props: Props) => {
           </div>
           <h3 className="text-3xl font-bold">Proven</h3>
           <p>Leading the Pc service world for 10 years</p>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
